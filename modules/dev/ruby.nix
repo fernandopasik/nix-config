4b41149fc2e2ddcd106f191fc9990882c3165e0f
Fernando Pasik <fernando@pasik.com.ar>
2025-05-24 10:49:35 +0000

    fix: ruby env variables

--- modules/dev/ruby.nix
+++ modules/dev/ruby.nix
@@ -20,7 +20,9 @@
     if [[ ! -d "$(rbenv root)/plugins/ruby-build" ]]; then
       git clone --quiet https://github.com/rbenv/ruby-build.git "$(rbenv root)/plugins/ruby-build"
     fi
-    export RUBY_CONFIGURE_OPTS="--with-libyaml-dir=${pkgs.libyaml} --with-openssl-dir=${pkgs.openssl.dev}"
+    export RUBY_CONFIGURE_OPTS="--with-libyaml-dir=${pkgs.libyaml} --with-openssl-dir=${pkgs.openssl.out}"
+    export CPPFLAGS="-I${pkgs.openssl.dev}/include"
+    export LDFLAGS="-I${pkgs.openssl.out}/lib"
     eval "$(rbenv init -)"
   '';
 }

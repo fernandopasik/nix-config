04b22765a48077b190409c223c9e6efaf98c4146
Fernando Pasik <fernando@pasik.com.ar>
2025-05-24 08:23:06 +0000

    fix: ruby dependencies path

--- modules/dev/ruby.nix
+++ modules/dev/ruby.nix
@@ -20,7 +20,7 @@
     if [[ ! -d "$(rbenv root)/plugins/ruby-build" ]]; then
       git clone --quiet https://github.com/rbenv/ruby-build.git "$(rbenv root)/plugins/ruby-build"
     fi
-    export RUBY_CONFIGURE_OPTS="--with-libyaml-dir=${pkgs.libyaml.dev} --with-openssl-dir=${pkgs.openssl.dev}"
+    export RUBY_CONFIGURE_OPTS="--with-libyaml-dir=${pkgs.libyaml} --with-openssl-dir=${pkgs.openssl}"
     eval "$(rbenv init -)"
   '';
 }

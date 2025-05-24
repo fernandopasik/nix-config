eb009b4e14923b58631d63a7d15d4e5bea43b5e2
Fernando Pasik <fernando@pasik.com.ar>
2025-05-24 05:43:25 +0000

    fix: install ruby plugin

--- modules/dev/ruby.nix
+++ modules/dev/ruby.nix
@@ -7,6 +7,9 @@
 
   programs.zsh.promptInit = ''
     export PATH="${pkgs.rbenv}/bin:$PATH"
+    if [[ ! -d "$(rbenv root)/plugins/ruby-build" ]]; then
+      git clone --quiet https://github.com/rbenv/ruby-build.git "$(rbenv root)/plugins/ruby-build"
+    fi
     eval "$(rbenv init -)"
   '';
 }

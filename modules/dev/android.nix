6d843e2471fc33ca03e2345e47fe01045f9b111a
Fernando Pasik <fernando@pasik.com.ar>
2025-05-25 16:35:20 +0000

    refactor: move block into let

--- modules/dev/android.nix
+++ modules/dev/android.nix
@@ -7,46 +7,49 @@
   pkgs,
   ...
 }:
-lib.mkMerge [
-  {
-    environment.systemPackages = [
-      (androidModule.sdk.${pkgs.system} (
-        sdkPkgs:
-        with sdkPkgs;
-        [
-          cmdline-tools-latest
-          platform-tools
-          emulator
 
-          # Multiple platforms
-          platforms-android-35
-          platforms-android-36
+lib.mkMerge (
+  let
+    androidSdkPkg = androidModule.sdk.${pkgs.system} (
+      sdkPkgs:
+      with sdkPkgs;
+      [
+        cmdline-tools-latest
+        platform-tools
+        emulator
 
-          # Multiple build tools
-          build-tools-35-0-0
-          build-tools-36-0-0
+        # Platforms
+        platforms-android-35
+        platforms-android-36
 
-        ]
-        ++ lib.optionals (pkgs.system == "aarch64-darwin") [
-          # System images (emulators)
-          system-images-android-35-google-apis-arm64-v8a
-          system-images-android-36-google-apis-arm64-v8a
-        ]
-        ++ lib.optionals (pkgs.system == "x86_64-linux" || pkgs.system == "x86_64-darwin") [
-          # System images (emulators)
-          system-images-android-35-google-apis-x86-64
-          system-images-android-36-google-apis-x86-64
-        ]
-      ))
-    ];
-  }
-  (lib.optionalAttrs isDarwin {
-    homebrew = {
-      casks = [
-        "android-commandlinetools"
-        "android-platform-tools"
-        "android-studio"
-      ];
-    };
-  })
-]
+        # Build tools
+        build-tools-35-0-0
+        build-tools-36-0-0
+
+      ]
+      ++ lib.optionals (pkgs.system == "aarch64-darwin") [
+        # System images (emulators)
+        system-images-android-35-google-apis-arm64-v8a
+        system-images-android-36-google-apis-arm64-v8a
+      ]
+      ++ lib.optionals (pkgs.system == "x86_64-linux" || pkgs.system == "x86_64-darwin") [
+        # System images (emulators)
+        system-images-android-35-google-apis-x86-64
+        system-images-android-36-google-apis-x86-64
+      ]
+    );
+  in
+  [
+    { environment.systemPackages = [ androidSdkPkg ]; }
+
+    (lib.optionalAttrs isDarwin {
+      homebrew = {
+        casks = [
+          "android-commandlinetools"
+          "android-platform-tools"
+          "android-studio"
+        ];
+      };
+    })
+  ]
+)

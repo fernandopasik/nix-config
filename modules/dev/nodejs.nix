af0777d11b0c5c1ceaea30bc7dbb2360ba467656
Fernando Pasik <fernando@pasik.com.ar>
2025-05-25 14:47:43 +0000

    fix: volta path

--- modules/dev/nodejs.nix
+++ modules/dev/nodejs.nix
@@ -16,4 +16,9 @@
     nodePackages_latest.npm-check-updates
     nodePackages_latest.yo
   ];
+
+  programs.zsh.promptInit = ''
+    export VOLTA_HOME="$HOME/.volta";
+    export PATH="$VOLTA_HOME/bin:$PATH"
+  '';
 }

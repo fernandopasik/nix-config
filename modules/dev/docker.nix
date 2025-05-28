f87d2b96bbfa8fc12e59c9e7570abef0c484c629
Fernando Pasik <fernando@pasik.com.ar>
2025-05-28 21:08:42 +0100

    feat: install docker in windows

--- modules/dev/docker.nix
+++ modules/dev/docker.nix
@@ -2,17 +2,24 @@
   config,
   isDarwin,
   isLinux,
+  isWSL,
   lib,
+  libx,
   pkgs,
   ...
 }:
 lib.mkMerge [
   (lib.optionalAttrs isDarwin { homebrew.casks = [ "docker" ]; })
+
   (lib.optionalAttrs isLinux {
     virtualisation.docker = {
       enable = true;
       rootless.enable = true;
       rootless.setSocketVariable = true;
     };
+  })
+
+  (lib.optionalAttrs isWSL {
+    system.activationScripts.postActivation.text = libx.installWingetPkgs [ "Docker.DockerDesktop" ];
   })
 ]

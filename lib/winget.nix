27b6ec313700be593b08714fcce7463d31a08c2d
Fernando Pasik <fernando@pasik.com.ar>
2025-05-28 20:27:57 +0100

    fix: winget script

--- lib/winget.nix
+++ lib/winget.nix
@@ -8,18 +8,20 @@
         let
           isStr = builtins.isString pkg;
           name = if isStr then pkg else pkg.name;
-          locationArg = if (!isStr && pkg ? location) then " --location=\"${pkg.location}\"" else "";
+          locationArg = if (!isStr && pkg ? location) then " --location=\\\"" + pkg.location + "\\\"" else "";
           winget = "winget install --accept-package-agreements --silent --disable-interactivity --include-unknown --exact";
-          script = ''
-            & {
-              ${winget} --id=\"${name}\"${locationArg};
-              if ([int]$LASTEXITCODE -band 0xFF -eq 43) { exit 0 } else { exit $LASTEXITCODE }
-            }
-          '';
         in
         ''
           echo "🖥️ Installing ${name}"
-          output=$(/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -NoProfile -Command '${script}')
+          output=$(/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -NoProfile -Command "& {
+            ${winget} --id=\"${name}\"${locationArg};
+            $code = [int]$LASTEXITCODE -band 0xFF
+            if ($code -eq 43) {
+              exit 0
+            } else {
+              exit $LASTEXITCODE
+            }
+          }")
           status=$?
           echo "PowerShell exit status: $status"
           echo "$output" | grep -v 'tcgetpgrp failed'

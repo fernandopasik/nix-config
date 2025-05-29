fd1da5d3af9f01304b9ad58421b48858bb19f5b5
Fernando Pasik <fernando@pasik.com.ar>
2025-05-29 13:14:37 +0000

    fix: char escaping

--- lib/winget.nix
+++ lib/winget.nix
@@ -8,14 +8,14 @@
         let
           isStr = builtins.isString pkg;
           name = if isStr then pkg else pkg.name;
-          locationArg = if (!isStr && pkg ? location) then " --location=\\\"" + pkg.location + "\\\"" else "";
+          locationArg = if (!isStr && pkg ? location) then " --location='" + pkg.location + "'" else "";
           winget = "winget install --accept-package-agreements --silent --disable-interactivity --include-unknown --exact";
         in
         ''
           echo "🖥️ Installing ${name}"
           output=$(/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -NoProfile -Command "& {
-            ${winget} --id=\"${name}\"${locationArg};
-            \$code = [int]\$LASTEXITCODE -band 0xFF
+            ${winget} --id='${name}'${locationArg};
+            \$code = [int]\$LASTEXITCODE -band 0xFF;
             if (\$code -eq 43) {
               exit 0
             } else {

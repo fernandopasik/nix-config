93d75c9e50ecef120509db7aedda3ab33aa4a9b8
Fernando Pasik <fernando@pasik.com.ar>
2025-10-22 20:35:40 +0000

    refactor: add more apps to module

--- modules/apps/security.nix
+++ modules/apps/security.nix
@@ -13,13 +13,21 @@ lib.mkMerge [
     homebrew.casks = [
       "1password"
       "nordvpn"
+      "yubico-authenticator"
+      "yubico-yubikey-manager"
     ];
+  })
+
+  (lib.optionalAttrs (isLinux || isWSL) {
+    environment.systemPackages = with pkgs; [ yubikey-manager ];
   })
 
   (lib.optionalAttrs isWSL {
     system.activationScripts.postActivation.text = libx.installWingetPkgs [
       "AgileBits.1Password"
       "NordSecurity.NordVPN"
+      "Yubico.Authenticator"
+      "Yubico.YubiKeyManagerCLI"
     ];
   })
 ]

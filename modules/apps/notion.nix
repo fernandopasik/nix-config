810b4403fa48909fafdca3f11ac41b4504c24107
Fernando Pasik <fernando@pasik.com.ar>
2025-10-25 07:25:40 +0100

    refactor: add notion to dock

--- modules/apps/notion.nix
+++ modules/apps/notion.nix
@@ -9,7 +9,11 @@
   ...
 }:
 lib.mkMerge [
-  (lib.optionalAttrs isDarwin { homebrew.casks = [ "notion" ]; })
+  (lib.optionalAttrs isDarwin {
+    homebrew.casks = [ "notion" ];
+
+    system.defaults.persistent-apps = [ "/Applications/Notion.app" ];
+  })
 
   (lib.optionalAttrs isWSL {
     system.activationScripts.postActivation.text = libx.installWingetPkgs [ "Notion.Notion" ];

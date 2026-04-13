272c5b8ab284b9c78cf7289c40157bb56a2d23c2
Fernando Pasik <fernando@pasik.com.ar>
2026-04-13 06:24:27 +0000

    fix(apps): dock app

--- apps/chatgpt.nix
+++ apps/chatgpt.nix
@@ -14,7 +14,7 @@ in
     lib.mkMerge [
       (lib.optionalAttrs isDarwin {
         environment.systemPackages = with pkgs; [ chatgpt ];
-        system.defaults.dock.persistent-apps = [ "/Applications/ChatGPT.app" ];
+        system.defaults.dock.persistent-apps = [ "/Applications/Nix Apps/ChatGPT.app" ];
       })
       (lib.optionalAttrs isWSL { winget.packages = [ "9nt1r1c2hh7j" ]; })
     ]

3a3ea140310d2d0806edad60c1abfe4cea5976c6
Fernando Pasik <fernando@pasik.com.ar>
2026-04-19 20:31:22 +0000

    refactor(apps): upgrade yubikey

--- apps/yubikey.nix
+++ apps/yubikey.nix
@@ -14,11 +14,32 @@ in
 {
   config = lib.mkIf enabled (
     lib.mkMerge [
-      { environment.systemPackages = with pkgs; [ yubikey-manager ]; }
+      {
+        environment.systemPackages = with pkgs; [
+          libfido2
+          yubikey-manager
+          yubikey-personalization
+        ];
+
+        programs.gnupg.agent = {
+          enable = true;
+          enableSSHSupport = true;
+        };
+
+        services.pcscd.enable = true;
+
+        services.udev.packages = with pkgs; [
+          yubikey-personalization
+          libu2f-host
+        ];
+      }
+
       (lib.optionalAttrs isDarwin { homebrew.casks = [ "yubico-authenticator" ]; })
+
       (lib.optionalAttrs (isLinux && !isHeadless) {
         environment.systemPackages = with pkgs; [ yubioath-flutter ];
       })
+
       (lib.optionalAttrs isWSL {
         winget.packages = [
           "Yubico.Authenticator"

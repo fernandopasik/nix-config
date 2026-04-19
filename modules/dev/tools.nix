d1644231763600887dd6c6b6507420033293dca6
Fernando Pasik <fernando@pasik.com.ar>
2026-04-19 20:23:01 +0000

    refactor(dev): console font

--- modules/dev/tools.nix
+++ modules/dev/tools.nix
@@ -10,26 +10,23 @@
 }:
 lib.mkMerge [
   {
-    environment.systemPackages = with pkgs; [
-      claude-code
-      gemini-cli
-      imagemagick
-      neovim
-      tree-sitter
-      watchman
-    ];
+    environment.systemPackages =
+      with pkgs;
+      [
+        claude-code
+        gemini-cli
+        imagemagick
+        neovim
+        nerd-fonts.ubuntu-mono
+        tree-sitter
+        watchman
+      ]
+      ++ lib.optionals isDarwin [ utm ];
+
     apps = [
       "ghostty"
       "vscode"
     ];
   }
-
-  (lib.optionalAttrs isDarwin {
-    environment.systemPackages = with pkgs; [ utm ];
-    homebrew = {
-      casks = [ "font-ubuntu-mono-nerd-font" ];
-    };
-  })
-
   (lib.optionalAttrs isWSL { winget.packages = [ "DEVCOM.JetBrainsMonoNerdFont" ]; })
 ]
